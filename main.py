6136df71f1f66e39eea426c16b3ba8331971419f
Kristobal <junta.kristobal@gmail.com>
2017-09-11 01:56:36 +0300

    Created basic functionality

---
+++ main.py
@@ -0,0 +1,90 @@
+# _*_ coding: utf-8 _*_
+
+from bs4 import BeautifulSoup
+from config import config
+import telebot
+import subprocess
+
+
+def parse_data():
+    parse_url = 'http://stat.candyrate.com.ua/'
+    subprocess.call('http --check-status --ignore-stdin --timeout=2.5 -p b get {} > resp.txt'.format(parse_url),
+                    shell=True)
+    resp = open('resp.txt')
+
+    soup = BeautifulSoup(resp.read(), 'html.parser')
+
+    table = soup.find_all('table')
+    if table:
+        table = table[0]
+    else:
+        raise Exception('Rating table not found')
+
+    keys = ['place', 'name', 'rating', 'place_avg', 'games', 'points', 'max', 'club']
+
+    data = list()
+
+    for row in table.tbody.find_all('tr'):
+        raw_row_data = [x.contents[0] for x in row.find_all('td')]
+        row_data = dict(zip(keys, raw_row_data))
+        row_data['name'] = row.a.contents[0]
+        data.append(row_data)
+
+    return data
+
+
+def form_row(row_data):
+    result = '{} *{}*: рейтинг - {}, игр - {}\n'.format(
+        (row_data['place']+'.').ljust(3, ' '),
+        row_data['name'],
+        row_data['rating'],
+        row_data['games'],
+    )
+    return result
+
+
+def form_table(table_data):
+    result = []
+    for row_data in table_data:
+        result.append(form_row(row_data))
+    return ''.join(result)
+
+
+bot = telebot.TeleBot(config.get('telegram-token'))
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    response = 'Добро пожаловать.'
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    response = ''.join([
+        'Это бот, сделанный для клуба "Ламповый Север"\n',
+        'Пока что он умеет только показывать стату - используй команды\n',
+        '/stats\n/stats nick'
+    ])
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=['stats'])
+def handle_stats(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    text = message.text.replace('/stats', '').strip()
+    stats = parse_data()
+    nick = text
+    print(nick)
+
+    if len(nick):
+        row = [x for x in stats if x['name'] == nick]
+        response = form_row(row[0]) if row else 'Такого игрока в таблице нет.'
+    else:
+        response = form_table(stats)
+
+    response += '\n[таблица](http://stat.candyrate.com.ua/)'
+    bot.send_message(message.chat.id, response, parse_mode='Markdown')
+
+
+bot.polling(none_stop=True, interval=0)
